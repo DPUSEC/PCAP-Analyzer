e23e08669e9569953e1170b3a5293f846f527680
Barış Azar <100796841+barisazar@users.noreply.github.com>
2025-02-16 00:29:27 +0300

    Update constants.go

--- backend/constants/constants.go
+++ backend/constants/constants.go
@@ -2,4 +2,4 @@ package constants
 
 const Version = "v0.2.0"
 
-var SecretKey = []byte("usmanim-nereye-gidersun-ebenin-korune-giderim-safiye")
+var SecretKey = []byte("VaRsA-SeKlINizz-cOmMiT-BeKleRiZ-TSSS")
